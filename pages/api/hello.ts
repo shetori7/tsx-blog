cb9bf7545b3ff9978365f2db6d358d74796b5f8e
shetori <shetori4818@gmail.com>
2024-08-16 18:01:42 +0900

    modified:   pages/api/hello.ts

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,8 +4,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 type Data = {
   name: string;
 };
-
-
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
